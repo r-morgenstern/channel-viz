ec572efd4c7bf3c753f2716869247ef79bb44fd6
r-morgenstern <mailbox@rene-morgenstern.de>
2014-04-25 12:01:25 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -146,8 +146,8 @@
 						 			// Build Graph
 									var graph = new Rickshaw.Graph( {
 										element: document.querySelector('#graph-' + feedId + '-' + datastream.id),
-										width: 600,
-										height: 200,
+										width: 950,
+										height: 400,
 										renderer: 'line',
 										min: parseFloat(datastream.min_value) - .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
 										max: parseFloat(datastream.max_value) + .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),

f3605dc65e6ebf057a409e1bcb9b235a860febce
r-morgenstern <mailbox@rene-morgenstern.de>
2014-04-25 12:22:13 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -151,7 +151,8 @@
 										renderer: 'line',
 										//min: parseFloat(datastream.min_value) - .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
 										min: 0,
-										max: parseFloat(datastream.max_value) + .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
+										//max: parseFloat(datastream.max_value) + .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
+										max: 100,
 										padding: {
 											top: 0.02,
 											right: 0.02,

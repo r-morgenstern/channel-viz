43d1ede5a0c9ede8ed88a775883980cb61c8f836
r-morgenstern <mailbox@rene-morgenstern.de>
2014-04-25 11:40:37 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,9 +12,9 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
+	var defaultKey		= 'EfEnVrIC5fdJFc6mMmaaDFhfI60ksgeGHpisQZESeAwuF8ll', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+		applicationName	= 'WiFi energy meter', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
